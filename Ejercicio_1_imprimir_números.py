35962ecb77f7731f5f818204d7f9d36dc7263c72
Angela <luzangelameloc1994@outlook.com>
2022-01-21 16:32:25 -0500

    Create Ejercicio_1_imprimir_números.py

---
+++ Ejercicio_1_imprimir_números.py
@@ -0,0 +1,40 @@
+#Imprimir los números entre 0 y 5
+
+n=0
+
+while n<=5:
+    print(n)
+    n = n + 1
+
+#Imprimir los números entre 1 y 5
+
+n=1
+
+while n<=5:
+    print(n)
+    n = n + 1
+
+#Imprimir los números entre 0 y 5
+
+n=0
+
+while n<=5:
+    print(n,end=" ")
+    n = n + 1
+
+#Imprimir los números desde 0 y preguntar si desea mostrar más números o no
+
+n=0
+opcion="S"
+
+while opcion=="S":
+    print(n)
+    n = n + 1
+    opcion=input("¿Desea mostrar más números? S/N ")
+print("\nPrograma Terminado")
+
+
+
+
+    
+
