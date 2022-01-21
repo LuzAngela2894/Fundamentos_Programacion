364803bd58a8b12b3dbdb8a168ca8dde7a03e124
Angela <luzangelameloc1994@outlook.com>
2022-01-21 16:36:43 -0500

    Create Tuplas_Diccionarios.py

---
+++ Tuplas_Diccionarios.py
@@ -0,0 +1,21 @@
+grupo = {}
+
+while True:
+    nombre = input("Ingresa el nombre del estudiante (o exit para detenerse): ")
+    if nombre == 'exit':
+        break
+    
+    calif = int(input("Ingresa la calificación del alumno (0-10): "))
+    
+    if nombre in grupo:
+        grupo[nombre] += (calif,)
+    else:
+        grupo[nombre] = (calif,)
+        
+for nombre in sorted(grupo.keys()):
+    sum = 0
+    contador = 0
+    for calif in sorted(grupo[nombre]):
+        sum += calif
+        contador += 1
+    print(nombre, ":", sum / contador)
