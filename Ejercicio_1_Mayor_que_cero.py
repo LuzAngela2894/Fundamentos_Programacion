4b1379e3b01f994de7aa9cfb287184f776594f30
Angela <luzangelameloc1994@outlook.com>
2022-01-21 16:32:28 -0500

    Create Ejercicio_1_Mayor_que_cero.py

---
+++ Ejercicio_1_Mayor_que_cero.py
@@ -0,0 +1,8 @@
+#Entrada
+numero=0
+#Proceso y Salida
+numero=float(input("\nIngresa por favor un número: "))
+if numero > 0:
+    print(numero,"\n¡Yei! Este número es mayor que cero")
+else:
+    print(numero,"\n¡D'oh! Este número es menor que cero")
