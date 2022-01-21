78c35b39cc56b572ebf1bcfcbffa29de895086fd
Angela <luzangelameloc1994@outlook.com>
2022-01-21 16:35:46 -0500

    Create Notas_1_Estudiante.py

---
+++ Notas_1_Estudiante.py
@@ -0,0 +1,17 @@
+#Algoritmo para encontrar la definitiva de un estudiante
+#Entradas
+nombre=" "
+asignatuta=" "
+nota1=0
+nota2=0
+definitiva=0
+#Procesamiento de la información
+nombre=input("\nIngrese el nombre del estudiante ")
+asignatura=input("\nIngrese el nombre de la asignatura ")
+nota1=float(input("\nIngrese la nota 1 "))
+nota2=float(input("\Ingrese la nota 2 "))
+definitiva=(nota1+nota2)/2
+#Salidas
+print("\nEl estudiante",nombre,"obutvo una definitiva de",definitiva,"en la asignatura",asignatura)
+      
+      
