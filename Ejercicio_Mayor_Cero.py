fdec55e90bf653a6f28c40c8fa97a2c7502e077d
Angela <luzangelameloc1994@outlook.com>
2022-01-21 16:34:31 -0500

    Create Ejercicio_Mayor_Cero.py

---
+++ Ejercicio_Mayor_Cero.py
@@ -0,0 +1,13 @@
+def mayor_que_cero():
+    numero = int(input("\nIngresar un numero: "))
+    if numero > 0:
+        print("\nEl número es mayor que cero")
+    else:
+        print("\nEl número no es mayor que cero")
+
+mayor_que_cero()
+
+
+
+        
+
