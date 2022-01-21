03178fda2c75799b0afb0b153d7a3f9d5ee2fa6b
Angela <luzangelameloc1994@outlook.com>
2022-01-21 16:30:03 -0500

    Create Archivos_Espacios.py

---
+++ Archivos_Espacios.py
@@ -0,0 +1,5 @@
+archivo = open("C:/Users/Luz Angela Melo/OneDrive - Universidad Piloto de Colombia/Documents/MinTic/Ciclo 1 Python/Semana 7/inbox.txt")
+for linea in archivo:
+    linea = linea.rstrip()
+    if linea.startswith("From:"):
+        print(linea)
