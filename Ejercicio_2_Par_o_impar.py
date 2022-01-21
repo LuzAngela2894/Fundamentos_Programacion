46bfd932f12463b7e022b295a71816d9e59d796e
Angela <luzangelameloc1994@outlook.com>
2022-01-21 16:32:35 -0500

    Create Ejercicio_2_Par_o_impar.py

---
+++ Ejercicio_2_Par_o_impar.py
@@ -0,0 +1,13 @@
+#Número es mayor que cero y es par
+numero=float(input("\nIngrese un número por favor: "))
+if numero>=0 and numero%2==0:
+    print("\n",numero,"Este número es mayor que cero y es par")
+else:
+   print("\n",numero,"Este número no es mayor que cero y no es par")
+   
+#Número es par o impar
+numero=float(input("\nIngrese un número por favor: "))
+if numero%2==0:
+    print("\n",numero,"Este número es par")
+else:
+    print("\n",numero,"Este número es impar")
