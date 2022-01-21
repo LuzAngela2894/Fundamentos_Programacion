3b1ebf5c9e4182b646ce8c7a555485e6283246e0
Angela <luzangelameloc1994@outlook.com>
2022-01-21 16:32:54 -0500

    Create Ejercicio_5_Rango_número.py

---
+++ Ejercicio_5_Rango_número.py
@@ -0,0 +1,6 @@
+#Rango número
+numero=float(input("\nIngrese un numero por favor: "))
+if numero>=10 and numero<=20:
+    print("\nEl número ingresado está entre el rango de 10 y 20")
+else:
+    print("\nEl número ingresado NO está entre el rango de 10 y 20")
