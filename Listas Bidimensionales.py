26a761416be00c9654f6aa4ba95b34884c7e01f4
Angela <luzangelameloc1994@outlook.com>
2022-01-21 16:35:13 -0500

    Create Listas Bidimensionales.py

---
+++ Listas Bidimensionales.py
@@ -0,0 +1,10 @@
+# Una tabla de cuatro columnas y cuatro filas: un arreglo bidimensional (4x4)
+
+tabla = [[":(", ":)", ":(", ":)"],
+         [":)", ":(", ":)", ":)"],
+         [":(", ":)", ":)", ":("],
+         [":)", ":)", ":)", ":("]]
+
+print(tabla)
+print(tabla [0][0]) # salida: ':('
+print(tabla [0][3]) # salida: ':)'
