48072ad8a2f37cd2092634d191c073a254c675ae
Angela <luzangelameloc1994@outlook.com>
2022-01-21 16:29:53 -0500

    Create Archivos_Contador.py

---
+++ Archivos_Contador.py
@@ -0,0 +1,5 @@
+archivo = open("C:/Users/Luz Angela Melo/OneDrive - Universidad Piloto de Colombia/Documents/MinTic/Ciclo 1 Python/Semana 7/inbox.txt")
+contador = 0
+for i in archivo:
+    contador += 1
+print("Contador de líneas:", contador)
