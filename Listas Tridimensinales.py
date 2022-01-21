88f0311000df7c69cd5072558700cbdf363002b8
Angela <luzangelameloc1994@outlook.com>
2022-01-21 16:35:17 -0500

    Create Listas Tridimensinales.py

---
+++ Listas Tridimensinales.py
@@ -0,0 +1,19 @@
+# Cubo - un arreglo tridimensional (3x3x3)
+
+cubo = [[[':(', 'x', 'x'],
+         [':)', 'x', 'x'],
+         [':(', 'x', 'x']],
+
+        [[':)', 'x', 'x'],
+         [':(', 'x', 'x'],
+         [':)', 'x', 'x']],
+
+        [[':(', 'x', 'x'],
+         [':)', 'x', 'x'],
+         [':)', 'x', 'x']]]
+
+print(cubo)
+print("\n")
+print(cubo [0][0][0]) # salida: ':('
+print("\n")
+print(cubo [2][2][0]) # salida: ':)'
