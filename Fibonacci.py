059ac8baf877f17ce17bbb3e556e36d241c1b30b
Angela <luzangelameloc1994@outlook.com>
2022-01-21 16:35:00 -0500

    Create Fibonacci.py

---
+++ Fibonacci.py
@@ -0,0 +1,15 @@
+def fib(n):
+    if n < 1:
+         return None
+    if n < 3:
+        return 1
+
+    elem1 = elem2 = 1
+    sum = 0
+    for i in range(3, n + 1):
+        sum = elem1 + elem2
+        elem1, elem2 = elem2, sum
+    return sum
+
+for n in range(1, 10):
+    print(n, "->", fib(n))
